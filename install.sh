b05ba1a1ff293ca9142a49e11c6f63c8d5a14972
Morgan Aldridge <morgant@makkintosshu.com>
2008-08-19 18:45:03 +0000

    Added files from Sysmon-StartupItem-0.2

--- install.sh
+++ install.sh
@@ -2,10 +2,15 @@
 
 #
 # install.sh - Install Script for Sysmon Startup Item
-# v0.1 by Morgan Aldridge (morgant@makkintosshu.com)
+# v0.1 by Morgan Aldridge <morgant@makkintosshu.com>
+# v0.2 by Morgan Aldridge <morgant@makkintosshu.com> (now installs to
+#	/Library/StartupItems/ instead of /System/Library/StartupItems/.
 #
 
 echo "Copying Sysmon to /System/Library/StartupItems/... "
-mkdir -p /System/Library/StartupItems/
-cp -rpn Sysmon /System/Library/StartupItems/
-echo "Done."
+mkdir -p /Library/StartupItems/
+cp -rpn Sysmon /Library/StartupItems/
+echo "  Done."
+echo "Setting ownership on /System/Library/StartupItems/Sysmon/... "
+chown -R root:wheel /Library/StartupItems/Sysmon/
+echo "  Done."
