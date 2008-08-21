62530901e3ff92fd9156abd44567b68c9969c167
Morgan Aldridge <morgant@makkintosshu.com>
2008-08-21 00:55:03 +0000

    Rewrote install.sh to determine which version of Darwin/Mac OS X is running and install (now using 'install -c' instead of 'cp') the StartupItem or launchd plist as appropriate.

--- install.sh
+++ install.sh
@@ -1,16 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
 #
-# install.sh - Install Script for Sysmon Startup Item
+# install.sh - Install script for Sysmon StartupItem/launchd job
 # v0.1 by Morgan Aldridge <morgant@makkintosshu.com>
 # v0.2 by Morgan Aldridge <morgant@makkintosshu.com> (now installs to
-#	/Library/StartupItems/ instead of /System/Library/StartupItems/.
+#	/Library/StartupItems/ instead of /System/Library/StartupItems/)
+# v0.3 by Morgan Aldridge <morgant@makkintosshu.com> (now installs a StartupItem
+#   under Panther & Tiger or a launchd plist under Leopard; better condition
+#   checks)
 #
 
-echo "Copying Sysmon to /System/Library/StartupItems/... "
-mkdir -p /Library/StartupItems/
-cp -rpn Sysmon /Library/StartupItems/
-echo "  Done."
-echo "Setting ownership on /System/Library/StartupItems/Sysmon/... "
-chown -R root:wheel /Library/StartupItems/Sysmon/
-echo "  Done."
+os=`sysctl -n kern.ostype`
+darwin_version=`sysctl -n kern.osrelease | cut -d . -f 1`
+dst='/Library'
+
+echo "Checking Darwin/Mac OS X version..."
+if [ $os = "Darwin" ]; then
+	if [ $darwin_version -eq 7 -o $darwin_version -eq 8 ]; then
+		echo "  Running Mac OS X 10.3 (Panther) or 10.4 (Tiger)."
+		echo "Installing Sysmon to $dst/StartupItems/..."
+		install -cd -m 750 -o root -g wheel Library/StartupItems/Sysmon $dst/StartupItems/
+		echo "  Done."
+	elif [ $darwin_version -eq 9 ]; then
+		echo "  Running Mac OS X 10.5 (Leopard)."
+		echo "Installing org.sysmon.sysmond.plist to $dst/LaunchDaemons/..."
+		install -cd -m 750 -o root -g wheel Library/LaunchDaemons/org.sysmon.sysmond.plist $dst/LaunchDaemons/
+		echo "  Done."
+	else
+		echo "FATAL ERROR: Not running a supported version of Darwin/Mac OS X. Please see the included README."
+	fi
+else
+	echo "FATAL ERROR: Not running Darwin/Mac OS X. Please see the included README."
+fi
